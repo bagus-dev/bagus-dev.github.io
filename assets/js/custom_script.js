4282c6a58f1597f210c974a48655b9b68137be3a
Bagus Puji Rahardjo <bagus.rahardjo6@gmail.com>
2021-03-16 21:19:30 +0700

    ubah lagi

--- assets/js/custom_script.js
+++ assets/js/custom_script.js
@@ -6,8 +6,8 @@
         let myWidth = window.innerWidth;
 
         if (myWidth > 767) {
-            $("#btn-pc").css("display","block");
-            $("#btn-mobile").css("display","none");
+            $(".btn-pc").css("display","block");
+            $(".btn-mobile").css("display","none");
             $(".foto-pribadi").addClass("img-fluid");
             $("#center-img").css("display","none");
             $("#img-bagus").css("display","block");
@@ -22,13 +22,13 @@
             $("#contact-link").removeClass("mt-3");
             $("#contact-link").addClass("mt-4");
         } else {
-            $("#btn-pc").css("display","none");
-            $("#btn-mobile").css("display","block");
+            $(".btn-pc").css("display","none");
+            $(".btn-mobile").css("display","block");
             $(".foto-pribadi").removeClass("img-fluid");
             $("#center-img").css("display","block");
             $("#img-bagus").css("display","none");
-            $(".text-1").removeClass("text-center");
-            $(".text-2").removeClass("text-center");
+            $(".text-1").addClass("text-center");
+            $(".text-2").addClass("text-center");
             $("#top-link").removeClass("mt-4");
             $("#top-link").addClass("mt-3");
             $("#intro-link").removeClass("mt-4");
