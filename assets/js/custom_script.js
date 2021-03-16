e1aa37b95b8bb2db3fef13aeada4cc24faf21881
Bagus Puji Rahardjo <bagus.rahardjo6@gmail.com>
2021-03-16 16:28:17 +0700

    jadi gini

--- assets/js/custom_script.js
+++ assets/js/custom_script.js
@@ -12,6 +12,7 @@
             $("#center-img").css("display","none");
             $("#img-bagus").css("display","block");
             $(".text-custom").removeClass("text-center");
+            $(".paragraph-1").removeClass("text-center");
             $("#top-link").removeClass("mt-3");
             $("#top-link").addClass("mt-4");
             $("#intro-link").removeClass("mt-3");
@@ -27,6 +28,7 @@
             $("#center-img").css("display","block");
             $("#img-bagus").css("display","none");
             $(".text-custom").addClass("text-center");
+            $(".paragraph-1").addClass("text-center");
             $("#top-link").removeClass("mt-4");
             $("#top-link").addClass("mt-3");
             $("#intro-link").removeClass("mt-4");
